1e03f0c6dbc22d73b89c4fae4d51ef573515cb94
CA <consolation.archaism@protonmail.com>
2019-07-10 13:20:04 +0100

    parse json from the connection handler

--- app/Main.hs
+++ app/Main.hs
@@ -54,39 +54,14 @@ main = do
     WS.runServer "127.0.0.1" 9160 $ application state
 
 application :: MVar ServerState -> WS.ServerApp
-
 application state pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     msg <- WS.receiveData conn
     clients <- readMVar state
-    case msg of
-        _   | not (prefix `T.isPrefixOf` msg) ->
-                WS.sendTextData conn ("Wrong announcement" :: Text)
-            | any ($ fst client)
-                [T.null, T.any isPunctuation, T.any isSpace] ->
-                    WS.sendTextData conn ("Name cannot " `mappend`
-                        "contain punctuation or whitespace, and " `mappend`
-                        "cannot be empty" :: Text)
-            | clientExists client clients ->
-                WS.sendTextData conn ("User already exists" :: Text)
-            | otherwise -> flip finally disconnect $ do
-               modifyMVar_ state $ \s -> do
-                   let s' = addClient client s
-                   WS.sendTextData conn $
-                       "Welcome! Users: " `mappend`
-                       T.intercalate ", " (map fst s)
-                   broadcast (fst client `mappend` " joined") s'
-                   return s'
-               talk client state
-          where
-            prefix     = "Hi! I am "
-            client     = (T.drop (T.length prefix) msg, conn)
-            disconnect = do
-                -- Remove client and return new state
-                s <- modifyMVar state $ \s ->
-                    let s' = removeClient client s in return (s', s')
-                broadcast (fst client `mappend` " disconnected") s
+    case (eitherDecode msg :: Either String RequestData) of
+      Left errorMsg -> WS.sendTextData conn (T.pack errorMsg)
+      Right command -> WS.sendTextData conn ("valid request given" :: Text)
 
 talk :: Client -> MVar ServerState -> IO ()
 talk (user, conn) state = forever $ do
