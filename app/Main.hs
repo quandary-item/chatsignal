042cf421f68ad08c24ed72f96ec52c55e02fe45d
C A <consolation.archaism@protonmail.com>
2019-07-18 13:12:08 +0100

    remove writer monad

--- app/Main.hs
+++ app/Main.hs
@@ -10,15 +10,14 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import Control.Monad (forM_, forever)
 import Control.Monad.Catch (finally)
 import Control.Monad.Except (liftEither, MonadError)
-import Control.Monad.Identity (Identity)
-import Control.Monad.Writer.Lazy
-import Control.Concurrent (MVar, newMVar, modifyMVar, readMVar)
+import Control.Concurrent (MVar, newMVar, modifyMVar, modifyMVar_, readMVar)
 import qualified Data.Text as T
 import Control.Monad.Reader
 import qualified Network.WebSockets as WS
 import Data.ByteString.UTF8 (toString)
 
 import UserID (UserID, makeRandomUserID)
+
 
 type Validation = ReaderT ServerState (Either String)
 
@@ -144,82 +143,74 @@ usernameIsTakenErrorMessage :: String
 usernameIsTakenErrorMessage = "Username is already taken by an existing user"
 
 
-disconnect :: UserID -> MVar ServerState -> WriterT [Response] IO ()
+disconnect :: UserID -> MVar ServerState -> IO ()
 disconnect userId' state = do
-  liftIO $ putStrLn "disconnect"
-  currentState <- liftIO $ readMVar state
-  liftIO $ putStrLn $ show currentState
-  
+  putStrLn "disconnect"
+  currentState <- readMVar state
+  putStrLn $ show currentState
 
   case Map.lookup userId' currentState of
     Nothing -> return ()
     Just client -> do
-      s <- liftIO $ modifyMVar state $ \s -> do
+      s <- modifyMVar state $ \s -> do
         let s' = removeClient userId' s
         return (s', s')
-      tell [Broadcast (ServerMessage $ username client `mappend` " disconnected") s]
+      sendResponse $ Broadcast (ServerMessage $ username client `mappend` " disconnected") s
 
 
-performRequestData :: (Monad m) => RequestData -> Client -> ServerState -> WriterT [Response] m ()
+performRequestData :: RequestData -> Client -> ServerState -> IO ()
 performRequestData (Ping targetId) client clients = do
   case Map.lookup targetId clients of
     Nothing         -> return ()
-    Just targetUser -> tell [Broadcast (ServerMessage $ username client `mappend` " pinged " `mappend` username targetUser) clients]
+    Just targetUser -> sendResponse $ Broadcast (ServerMessage $ username client `mappend` " pinged " `mappend` username targetUser) clients
 performRequestData (Say message) client clients = do
-  tell [Broadcast (ServerMessage $ username client `mappend` ": " `mappend` message) clients]
+  sendResponse $ Broadcast (ServerMessage $ username client `mappend` ": " `mappend` message) clients
 
 
-talk :: Client -> MVar ServerState -> WriterT [Response] IO ()
+talk :: Client -> MVar ServerState -> IO ()
 talk client state = do
-  msg <- liftIO . WS.receiveData $ (connection client)
+  msg <- WS.receiveData $ (connection client)
 
-  clients <- liftIO . readMVar $ state
+  clients <- readMVar $ state
 
-  let responses = case runReaderT (ingestData msg) clients of
-        Left errorMsg -> [Response (ServerMessage $ T.pack errorMsg) (connection client)]
-        Right command -> execWriter (performRequestData command client clients)
-
-  tell responses
+  case runReaderT (ingestData msg) clients of
+    Left errorMsg -> sendResponse $ Response (ServerMessage $ T.pack errorMsg) (connection client)
+    Right command -> performRequestData command client clients
 
 
-performConnectRequestData :: ConnectRequestData -> WS.Connection -> MVar ServerState -> WriterT [Response] IO ()
+
+performConnectRequestData :: ConnectRequestData -> WS.Connection -> MVar ServerState -> IO ()
 performConnectRequestData (Connect providedUsername) conn state = do
   -- Create a user id
-  newUserId <- liftIO $ makeRandomUserID
+  newUserId <- makeRandomUserID
   -- Create the actual client
   let client = Client { username = providedUsername, userId = newUserId, connection = conn }
 
   (serveConnection client state) `finally` (disconnect (newUserId) state)
 
 
-connectClient :: Client -> ServerState -> WriterT [Response] Identity ServerState
+connectClient :: Client -> ServerState -> IO ServerState
 connectClient client clients = do
   -- Send a welcome / motd
-  tell [Response (ServerMessage "Welcome to One Hour Chat!") (connection client)]
+  sendResponse $ Response (ServerMessage "Welcome to One Hour Chat!") (connection client)
   -- Tell the client what their user id is
-  tell [Response (ConnectionNotify (userId client)) (connection client)]
+  sendResponse $ Response (ConnectionNotify (userId client)) (connection client)
 
   -- Add the new client to the state
   let newClients = addClient client clients
 
   -- Notify everyone that the party has officially started
-  tell [Broadcast (ServerMessage $ username client `mappend` " joined") clients]
-  tell [Broadcast (ServerStateResponse newClients) newClients]
+  sendResponse $ Broadcast (ServerMessage $ username client `mappend` " joined") clients
+  sendResponse $ Broadcast (ServerStateResponse newClients) newClients
 
   -- return the updated list of clients
   pure newClients
 
 
-serveConnection :: Client -> MVar ServerState -> WriterT [Response] IO ()
+serveConnection :: Client -> MVar ServerState -> IO ()
 serveConnection client state = do
-  -- Connect the client while collecting response messages
-  responses <- liftIO . modifyMVar state $ \clients -> do
-    let (newClients, responses) = runWriter $ connectClient client clients
-    pure (newClients, responses)
-
-  liftIO $ putStrLn $ show responses
-  -- Push the responses out onto this monad
-  tell responses
+  -- Connect the client by updating the state
+  modifyMVar_ state $ connectClient client 
 
   -- Serve subsequent requests for this client
   forever $ talk client state
@@ -244,12 +235,9 @@ application state pending = do
 
     clients <- readMVar state
 
-    responses <- case runReaderT (ingestData msg) clients of
-          Left errorMsg -> pure [Response (ServerMessage $ T.pack errorMsg) conn]
-          Right command -> execWriterT (performConnectRequestData command conn state)
-    putStrLn "end of responses..."
-    -- send the response/broadcasts
-    mapM_ sendResponse responses
+    case runReaderT (ingestData msg) clients of
+      Left errorMsg -> sendResponse $ Response (ServerMessage $ T.pack errorMsg) conn
+      Right command -> performConnectRequestData command conn state
 
 
 main :: IO ()
