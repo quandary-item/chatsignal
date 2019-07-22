daa88e3a0e53382aa16e29d1e2c1bfe6da72eb18
C A <consolation.archaism@protonmail.com>
2019-07-22 14:51:41 +0100

    lift up response type into a typeclass

--- app/Main.hs
+++ app/Main.hs
@@ -31,17 +31,17 @@ class Performable r a | r -> a where
   perform :: r -> MVar ServerState -> ReaderT a IO ()
 
 
-sendSingle :: (ToJSON a, Show a) => a -> WS.Connection -> IO ()
+sendSingle :: (JSONResponse a, Show a) => a -> WS.Connection -> IO ()
 sendSingle responseData conn = do
   putStrLn $ "Response: " ++ show responseData
   WS.sendTextData conn message
-    where message = encode responseData
+    where message = encode $ WrapToJSON responseData
 
-sendBroadcast :: (ToJSON a, Show a) => a -> ServerState -> IO ()
+sendBroadcast :: (JSONResponse a, Show a) => a -> ServerState -> IO ()
 sendBroadcast responseData clients = do
   putStrLn $ "Broadcast: " ++ show responseData
   forM_ clients $ \client -> WS.sendTextData (connection client) message
-    where message = encode responseData
+    where message = encode $ WrapToJSON responseData
 
 
 data ConnectRequestData = Connect T.Text deriving Show
@@ -139,46 +139,79 @@ instance Performable RequestData (Client, ServerState) where
       Just targetUser -> liftIO $ sendSingle (RejectCallResponse (userId client)) (connection targetUser)
 
 
-data ResponseData = ServerStateResponse ServerState
-                  | ServerMessage T.Text
-                  | ConnectionNotify UserID
-                  | OfferSDPResponse UserID SDPData
-                  | SendICEResponse UserID ICECandidate
-                  | StartCallResponse UserID
-                  | AcceptCallResponse UserID
-                  | RejectCallResponse UserID
-instance Show ResponseData where
+data ServerStateResponse = ServerStateResponse ServerState
+data ServerMessage = ServerMessage T.Text
+data ConnectionNotify = ConnectionNotify UserID
+data OfferSDPResponse = OfferSDPResponse UserID SDPData
+data SendICEResponse = SendICEResponse UserID ICECandidate
+data StartCallResponse = StartCallResponse UserID
+data AcceptCallResponse = AcceptCallResponse UserID
+data RejectCallResponse = RejectCallResponse UserID
+
+instance Show ServerStateResponse where
   show (ServerStateResponse clients) = "Clients: " ++ (T.unpack $ T.intercalate ", " $ map (T.pack . show) $ clientIdList clients)
+
+instance Show ServerMessage where
   show (ServerMessage text)          = "Message: " ++ T.unpack text
+
+instance Show ConnectionNotify where
   show (ConnectionNotify userId')    = "Notify: "  ++ show userId'
+
+instance Show OfferSDPResponse where
   show (OfferSDPResponse fromId _)   = "Offer SDP Response from " ++ show fromId
+
+instance Show SendICEResponse where
   show (SendICEResponse  fromId iceData)   = "Send ICE Candidate '" ++ (show iceData) ++ "' from " ++ show fromId
+
+instance Show StartCallResponse where
   show (StartCallResponse fromId)    = "Start Call from " ++ show fromId
+
+instance Show AcceptCallResponse where
   show (AcceptCallResponse fromId)   = "Accept Call from " ++ show fromId
+
+instance Show RejectCallResponse where
   show (RejectCallResponse fromId)   = "Reject Call from " ++ show fromId
 
-kind :: ResponseData -> T.Text
-kind (ServerStateResponse _) = "clients"
-kind (ServerMessage _) = "message"
-kind (ConnectionNotify _) = "notify"
-kind (OfferSDPResponse _ _) = "offer"
-kind (SendICEResponse _ _) = "ice"
-kind (StartCallResponse _) = "startcall"
-kind (AcceptCallResponse _) = "acceptcall"
-kind (RejectCallResponse _) = "rejectcall"
 
-toJSON' :: KeyValue a => ResponseData -> [a]
-toJSON' (ServerStateResponse clients)     = [ "clients" .= clientList clients ]
-toJSON' (ServerMessage text)              = [ "data" .= text ]
-toJSON' (ConnectionNotify userId')        = [ "user_id" .= show userId' ]
-toJSON' (OfferSDPResponse fromId sdpData) = [ "from" .= show fromId, "sdp"  .= sdpData ]
-toJSON' (SendICEResponse fromId iceData)  = [ "from" .= show fromId, "ice"  .= iceData ]
-toJSON' (StartCallResponse fromId)        = [ "from" .= show fromId ]
-toJSON' (AcceptCallResponse fromId)        = [ "from" .= show fromId ]
-toJSON' (RejectCallResponse fromId)        = [ "from" .= show fromId ]
+class JSONResponse a where
+  kind :: a -> T.Text
+  toValue :: KeyValue b => a -> [b]
 
-instance ToJSON ResponseData where
-  toJSON responseData = object $ [ "kind" .= kind responseData ] ++ toJSON' responseData
+instance JSONResponse ServerStateResponse where
+  kind _ = "clients"
+  toValue (ServerStateResponse clients)     = [ "clients" .= clientList clients ]
+instance JSONResponse ServerMessage where
+  kind _ = "message"
+  toValue (ServerMessage text)              = [ "data" .= text ]
+
+instance JSONResponse ConnectionNotify where
+  kind _ = "notify"
+  toValue (ConnectionNotify userId')        = [ "user_id" .= show userId' ]
+
+instance JSONResponse OfferSDPResponse where
+  kind _ = "offer"
+  toValue (OfferSDPResponse fromId sdpData) = [ "from" .= show fromId, "sdp"  .= sdpData ]
+
+instance JSONResponse SendICEResponse where
+  kind _ = "ice"
+  toValue (SendICEResponse fromId iceData)  = [ "from" .= show fromId, "ice"  .= iceData ]
+
+instance JSONResponse StartCallResponse where
+  kind _ = "startcall"
+  toValue (StartCallResponse fromId)        = [ "from" .= show fromId ]
+
+instance JSONResponse AcceptCallResponse where
+  kind _ = "acceptcall"
+  toValue (AcceptCallResponse fromId)        = [ "from" .= show fromId ]
+
+instance JSONResponse RejectCallResponse where
+  kind _ = "rejectcall"
+  toValue (RejectCallResponse fromId)        = [ "from" .= show fromId ]
+
+newtype WrappedToJSON a = WrapToJSON { unwrapToJSON :: a }
+
+instance (JSONResponse a) => ToJSON (WrappedToJSON a) where
+  toJSON (WrapToJSON responseData) = object $ [ "kind" .= kind responseData ] ++ toValue responseData
 
 
 isValidUsername :: T.Text -> Bool
