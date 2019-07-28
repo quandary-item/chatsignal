3ae4082b22a12924e2ce2445e27c44fd981765a5
C A <consolation.archaism@protonmail.com>
2019-07-28 13:06:57 +0100

    only serve WS from this server

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Application (createInitialState, application, MutableServerState)
+import Application (createInitialState, application)
 
 import Network.WebSockets.Snap
-import Snap.Core
 import Snap.Http.Server
-import Snap.Util.FileServe
-
-site :: MutableServerState -> Snap ()
-site state = route
-  [ ("/api", serveDirectory "static")
-  , ("/static", serveApi state)
-  ]
-
-serveApi :: MutableServerState -> Snap ()
-serveApi state = runWebSocketsSnap $ application state
 
 main :: IO ()
 main = do
     state <- createInitialState
 
-    quickHttpServe $ site state
+    quickHttpServe $ runWebSocketsSnap $ application state
