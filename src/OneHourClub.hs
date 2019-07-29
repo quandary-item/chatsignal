6fac16685e4fea5e90937dacadebad6ea1acd7ca
C A <consolation.archaism@protonmail.com>
2019-07-29 12:04:52 +0100

    add functions for determining whether 1hc is open

---
+++ src/OneHourClub.hs
@@ -0,0 +1,29 @@
+import Data.Time.Clock
+import Data.Time.LocalTime
+import Data.Time.Zones
+import Data.Time.Zones.All (tzByLabel, TZLabel(Europe__London))
+
+currentLocalTimeByLabel :: TZ -> IO LocalTime
+currentLocalTimeByLabel localTZ = do
+  currentTime <- getCurrentTime
+  pure $ utcToLocalTimeTZ localTZ currentTime
+
+startTimeOfDay :: TimeOfDay
+startTimeOfDay = TimeOfDay { todHour = 6, todMin = 0, todSec = 0 }
+
+endTimeOfDay :: TimeOfDay
+endTimeOfDay = TimeOfDay { todHour = 7, todMin = 0, todSec = 0 }
+
+timeOfDayIsOpen :: TimeOfDay -> Bool
+timeOfDayIsOpen tod
+  | tod < startTimeOfDay = False
+  | tod > endTimeOfDay   = False
+  | otherwise            = True
+
+
+isOpen :: IO Bool
+isOpen = do
+  let localTZ = tzByLabel Europe__London
+  LocalTime { localTimeOfDay = currentTimeOfDay } <-
+    currentLocalTimeByLabel localTZ
+  pure $ timeOfDayIsOpen currentTimeOfDay
