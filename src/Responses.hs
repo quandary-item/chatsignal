72708d78cad733f0bafb01bfacf71cf202cd8e37
C A <consolation.archaism@protonmail.com>
2019-07-29 14:37:45 +0100

    use more generics in response format to simplify code, update response format

--- src/Responses.hs
+++ src/Responses.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Responses where
@@ -5,107 +8,74 @@ module Responses where
 import Control.Monad (forM_)
 import Data.Aeson
 import UserID (UserID)
-import ServerState (ServerState, clientIdList, clientList, connection)
+import ServerState (ServerState, connection)
 import qualified Data.Text as T
+import GHC.Generics
 import qualified Network.WebSockets as WS
 
 import WebRTC(ICECandidate, SDPData)
 
-newtype WrappedToJSON a = WrapToJSON { unwrapToJSON :: a }
+data Response a = Response { kind :: T.Text, content :: a } deriving (Generic, ToJSON, Show)
 
-class JSONResponse a where
+class HasKind a where
   kind :: a -> T.Text
-  toValue :: KeyValue b => a -> [b]
 
-instance (JSONResponse a) => ToJSON (WrappedToJSON a) where
-  toJSON (WrapToJSON responseData) = object
-    $ [ "kind" .= kind responseData ] ++ toValue responseData
+data ServerStateResponse = ServerStateResponse { clients :: ServerState } deriving (ToJSON, Generic)
+data ServerMessage = ServerMessage { message :: T.Text } deriving (ToJSON, Generic)
+data ConnectionNotify = ConnectionNotify { user_id :: UserID } deriving (ToJSON, Generic)
+data OfferSDPResponse = OfferSDPResponse { from :: UserID, sdp :: SDPData } deriving (ToJSON, Generic)
+data SendICEResponse = SendICEResponse { from :: UserID, ice :: ICECandidate } deriving (ToJSON, Generic)
+data StartCallResponse = StartCallResponse { from :: UserID } deriving (ToJSON, Generic)
+data AcceptCallResponse = AcceptCallResponse { from :: UserID } deriving (ToJSON, Generic)
+data RejectCallResponse = RejectCallResponse { from :: UserID } deriving (ToJSON, Generic)
+data BannedResponse = BannedResponse deriving (ToJSON, Generic)
 
-data ServerStateResponse = ServerStateResponse ServerState
-data ServerMessage = ServerMessage T.Text
-data ConnectionNotify = ConnectionNotify UserID
-data OfferSDPResponse = OfferSDPResponse UserID SDPData
-data SendICEResponse = SendICEResponse UserID ICECandidate
-data StartCallResponse = StartCallResponse UserID
-data AcceptCallResponse = AcceptCallResponse UserID
-data RejectCallResponse = RejectCallResponse UserID
-data BannedResponse = BannedResponse
-
-
-instance Show ServerStateResponse where
-  show (ServerStateResponse clients) = "Clients: " ++ (T.unpack $ T.intercalate ", " $ map (T.pack . show) $ clientIdList clients)
-
-instance Show ServerMessage where
-  show (ServerMessage text)          = "Message: " ++ T.unpack text
-
-instance Show ConnectionNotify where
-  show (ConnectionNotify userId')    = "Notify: "  ++ show userId'
-
-instance Show OfferSDPResponse where
-  show (OfferSDPResponse fromId _)   = "Offer SDP Response from " ++ show fromId
-
-instance Show SendICEResponse where
-  show (SendICEResponse  fromId iceData)   = "Send ICE Candidate '" ++ (show iceData) ++ "' from " ++ show fromId
-
-instance Show StartCallResponse where
-  show (StartCallResponse fromId)    = "Start Call from " ++ show fromId
-
-instance Show AcceptCallResponse where
-  show (AcceptCallResponse fromId)   = "Accept Call from " ++ show fromId
-
-instance Show RejectCallResponse where
-  show (RejectCallResponse fromId)   = "Reject Call from " ++ show fromId
-
-instance Show BannedResponse where
-  show (BannedResponse) = "User is banned"
-
-instance JSONResponse ServerStateResponse where
+instance HasKind ServerStateResponse where
   kind _ = "clients"
-  toValue (ServerStateResponse clients)     = [ "clients" .= clientList clients ]
 
-instance JSONResponse ServerMessage where
+instance HasKind ServerMessage where
   kind _ = "message"
-  toValue (ServerMessage text)              = [ "data" .= text ]
 
-instance JSONResponse ConnectionNotify where
+instance HasKind ConnectionNotify where
   kind _ = "notify"
-  toValue (ConnectionNotify userId')        = [ "user_id" .= show userId' ]
 
-instance JSONResponse OfferSDPResponse where
+instance HasKind OfferSDPResponse where
   kind _ = "offer"
-  toValue (OfferSDPResponse fromId sdpData) = [ "from" .= show fromId, "sdp"  .= sdpData ]
 
-instance JSONResponse SendICEResponse where
+instance HasKind SendICEResponse where
   kind _ = "ice"
-  toValue (SendICEResponse fromId iceData)  = [ "from" .= show fromId, "ice"  .= iceData ]
 
-instance JSONResponse StartCallResponse where
+instance HasKind StartCallResponse where
   kind _ = "startcall"
-  toValue (StartCallResponse fromId)        = [ "from" .= show fromId ]
 
-instance JSONResponse AcceptCallResponse where
+instance HasKind AcceptCallResponse where
   kind _ = "acceptcall"
-  toValue (AcceptCallResponse fromId)        = [ "from" .= show fromId ]
 
-instance JSONResponse RejectCallResponse where
+instance HasKind RejectCallResponse where
   kind _ = "rejectcall"
-  toValue (RejectCallResponse fromId)        = [ "from" .= show fromId ]
 
-instance JSONResponse BannedResponse where
+instance HasKind BannedResponse where
   kind _ = "banned"
-  toValue (BannedResponse) = []
-
 
 -- Methods for returning responses
 
-sendSingle :: (JSONResponse a, Show a) => a -> WS.Connection -> IO ()
-sendSingle responseData conn = do
-  putStrLn $ "Response: " ++ show responseData
-  WS.sendTextData conn message
-    where message = encode $ WrapToJSON responseData
+makeResponse :: (HasKind a) => a -> Response a
+makeResponse a = Response { kind = kind a, content = a }
 
-sendBroadcast :: (JSONResponse a, Show a) => a -> ServerState -> IO ()
-sendBroadcast responseData clients = do
+sendSingle :: (HasKind a, Show a, ToJSON a) => a -> WS.Connection -> IO ()
+sendSingle a = sendSingle' (makeResponse a)
+
+sendBroadcast :: (HasKind a, Show a, ToJSON a) => a -> ServerState -> IO ()
+sendBroadcast a = sendBroadcast' (makeResponse a)
+
+sendSingle' :: (Show a, ToJSON a) => Response a -> WS.Connection -> IO ()
+sendSingle' responseData conn = do
+  putStrLn $ "Response: " ++ show responseData
+  WS.sendTextData conn message'
+    where message' = encode $ responseData
+
+sendBroadcast' :: (Show a, ToJSON a) => Response a -> ServerState -> IO ()
+sendBroadcast' responseData clients' = do
   putStrLn $ "Broadcast: " ++ show responseData
-  forM_ clients $ \client -> WS.sendTextData (connection client) message
-    where message = encode $ WrapToJSON responseData
+  forM_ clients' $ \client -> WS.sendTextData (connection client) message'
+    where message' = encode $ responseData
